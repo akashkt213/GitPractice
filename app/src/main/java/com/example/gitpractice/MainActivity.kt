4e391b61d6e09c0f0d9eda58f05a5ca6cce4f198
Akash <akashkt213@gmail.com>
2021-11-09 18:01:09 +0530

    Revert "rris is er third commit"
    This reverts commit cfc798d285aceb816b3fada36ef9e62626222bb1.

--- app/src/main/java/com/example/gitpractice/MainActivity.kt
+++ app/src/main/java/com/example/gitpractice/MainActivity.kt
@@ -8,7 +8,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("This is the content of my second commit.")
-
-        this line is for making a mess nd reverting back.
     }
 }
